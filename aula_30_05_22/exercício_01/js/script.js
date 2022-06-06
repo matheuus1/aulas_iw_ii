274a84e11c97640b7754d208b17d2ccbd7c66c6e
Matheus Nascimento <80324098+teteu1n@users.noreply.github.com>
2022-06-06 08:37:44 -0300

    Add files via upload

---
+++ aula_30_05_22/exercício_01/js/script.js
@@ -0,0 +1,9 @@
+var ano = parseInt(document.getElementById('ano'))
+var sex = document.getElementById('sex')
+var botao = document.getElementById('calcular')
+
+botao.onclick = function () {
+    if (sex == "Masculino") {
+        document.write = ("Flamengo")
+    }
+}
